4f2ce95685cf1c862033eeeed9c1d2f4b83c9b16
MatthewGerber <gerber.matthew@gmail.com>
2016-02-24 08:06:00 -0500

    Added imports.

--- src/sys6018/examples/interchange/ParseUsers.java
+++ src/sys6018/examples/interchange/ParseUsers.java
@@ -2,6 +2,7 @@ package sys6018.examples.interchange;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -9,6 +10,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
