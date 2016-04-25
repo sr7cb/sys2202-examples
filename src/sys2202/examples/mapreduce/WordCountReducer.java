2e471eea6820b222cda35ac9e3586ef1952f548f
MatthewGerber <gerber.matthew@gmail.com>
2016-04-24 22:30:53 -0400

    Added word count mapper and reducer.

---
+++ src/sys2202/examples/mapreduce/WordCountReducer.java
@@ -0,0 +1,39 @@
+package sys2202.examples.mapreduce;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class WordCountReducer {
+
+	public static void main(String[] args) {
+		
+		// create a scanner to read lines from the standard input
+		Scanner inputScanner = new Scanner(System.in);
+
+		// reduce all word counts into an overall hashtable
+		Hashtable<String, Integer> wordCount = new Hashtable<String, Integer>();
+
+		// read each line on standard input
+		while (inputScanner.hasNextLine()) {
+			
+			// each line contains a word and its count from a mapper
+			String line = inputScanner.nextLine();
+			String[] splitLine = line.split("\t");
+			String word = splitLine[0];
+			int count = Integer.parseInt(splitLine[1]);
+			
+			// accumulate the word and count into our hashtable
+			if (wordCount.containsKey(word)) {
+				int currentCount = wordCount.get(word);
+				wordCount.put(word, currentCount + count);
+			} else
+				wordCount.put(word, 1);
+		}
+
+		inputScanner.close();
+
+		// output final counts to standard output
+		for (String word : wordCount.keySet())
+			System.out.println(word + "\t" + wordCount.get(word));
+	}
+}
